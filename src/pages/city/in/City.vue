d9cbd3d3c3c61892ad1a53afde4e76c74ac07e01
vernonn <695035427@qq.com>
2018-04-25 05:38:58 +0800

    TODO:city list

--- src/pages/city/in/City.vue
+++ src/pages/city/in/City.vue
@@ -2,18 +2,21 @@
   <div class="city-page">
     <city-header></city-header>
     <city-search></city-search>
+    <city-list></city-list>
   </div>
 </template>
 
 <script>
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
+import CityList from './components/List'
 
 export default {
   name: 'CityIn',
   components: {
     CityHeader,
-    CitySearch
+    CitySearch,
+    CityList
   }
 }
 </script>

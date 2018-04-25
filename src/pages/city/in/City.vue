e2a77cefd4c6fa58da24ee0dbdf7bdacc5f74f5f
vernonn <695035427@qq.com>
2018-04-25 18:11:14 +0800

    add city list

--- src/pages/city/in/City.vue
+++ src/pages/city/in/City.vue
@@ -2,7 +2,7 @@
   <div class="city-page">
     <city-header></city-header>
     <city-search></city-search>
-    <city-list></city-list>
+    <city-list :city="city" :hotCities="hotCities" :cities="cities"></city-list>
   </div>
 </template>
 
@@ -10,6 +10,7 @@
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
 import CityList from './components/List'
+import axios from 'axios'
 
 export default {
   name: 'CityIn',
@@ -17,6 +18,39 @@ export default {
     CityHeader,
     CitySearch,
     CityList
+  },
+  data () {
+    return {
+      city: '未定位',
+      hotCities: [],
+      cities: {}
+    }
+  },
+  methods: {
+    getCityInfo () {
+      axios.get('/api/city.json')
+        .then(this.getCityInfoSuccess)
+      axios.get('/api/index.json')
+        .then(this.getIndexInfoSuccess)
+    },
+    getCityInfoSuccess (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.hotCities = data.hotCities
+        this.cities = data.cities
+      }
+    },
+    getIndexInfoSuccess (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.city = data.city
+      }
+    }
+  },
+  mounted () {
+    this.getCityInfo()
   }
 }
 </script>
--- src/pages/city/in/components/List.vue
+++ src/pages/city/in/components/List.vue
@@ -2,23 +2,93 @@
   <div class="list">
     <div class="area">
       <div class="title">当前城市</div>
+      <div class="area-city">
+        <div class="city-button location-city">{{this.city}}</div>
+      </div>
     </div>
     <div class="area">
       <div class="title">热门城市</div>
+      <div class="area-city">
+        <div class="city-button" v-for="city of hotCities" :key="city.id">{{city.name}}</div>
+      </div>
     </div>
     <div class="area">
-      <div class="title">A</div>
+      <div class="cities">
+        <div class="city-character-list" v-for="(val,key) of cities" :key="key">
+          <div class="title">{{key}}</div>
+          <div class="area-city">
+            <div class="city-item border-topbottom" v-for="item of val" :key="item.id">{{item.name}}</div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'CityList'
+  name: 'CityList',
+  props: {
+    city: String,
+    hotCities: Array,
+    cities: Object
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 @import "~styles/variable.scss";
 
+.list {
+  .area {
+    .title {
+      height:.24rem;
+      line-height: .24rem;
+      font-size: .26rem;
+      padding: .24rem .3rem;
+      background: $homeBgColor;
+    }
+    .area-city {
+      padding: .1rem .5rem .1rem .24rem;
+      overflow: hidden;
+      .location-city {
+        background: $bgColor;
+        color: #fff;
+        // border-color: #fff!important;
+      }
+      .city-button {
+        float: left;
+        height: .4rem;
+        line-height: .4rem;
+        font-size: .26rem;
+        width:29%;
+        text-align: center;
+        margin: .1rem;
+        border: .02rem solid #ccc;
+        border-radius: .06rem;
+      }
+    }
+    .cities {
+      .city-character-list {
+        .area-city {
+          padding: 0;
+          .city-item {
+            height: .5rem;
+            line-height: .5rem;
+            font-size: .26rem;
+            padding: .1rem .5rem .1rem .34rem;
+          }
+          .border-topbottom {
+            &:bofore {
+              border-color: #ccc;
+            }
+            &:after {
+              border-color: #ccc;
+            }
+          }
+        }
+      }
+    }
+  }
+}
 </style>

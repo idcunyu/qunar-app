10e1ad35f8186a8d70364930eb82a29ad22a4022
vernonn <695035427@qq.com>
2018-04-26 13:31:26 +0800

    use keep-alive

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-    <router-view/>
+    <!-- 使用vue自带的keep-alive包裹router-view,重新进入路由,不用再重新加载组件,重新渲染页面 -->
+    <keep-alive>
+      <router-view/>
+    </keep-alive>
   </div>
 </template>
 
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -16,6 +16,7 @@ import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
 import axios from 'axios'
+import { mapState } from 'vuex'
 
 export default {
   name: 'Home',
@@ -28,15 +29,21 @@ export default {
   },
   data () {
     return {
+      lastCity: '',
       swiperList: [],
       iconList: [],
       recommendList: [],
       weekendList: []
     }
   },
+  computed: {
+    ...mapState({
+      currentCity: 'city'
+    })
+  },
   methods: {
     getHomeInfo () {
-      axios.get('/api/index.json')
+      axios.get('/api/index.json?city=' + this.currentCity)
         .then(this.getHomeInfoSuccess)
     },
     getHomeInfoSuccess (res) {
@@ -51,7 +58,14 @@ export default {
     }
   },
   mounted () {
+    this.lastCity = this.currentCity
     this.getHomeInfo()
+  },
+  activated () {
+    if (this.lastCity !== this.currentCity) {
+      this.lastCity = this.currentCity
+      this.getHomeInfo()
+    }
   }
 }
 </script>

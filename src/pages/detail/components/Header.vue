cea6aba0af4c662da5297ca74d351a6850beaad8
vernonn <695035427@qq.com>
2018-04-28 18:42:39 +0800

    fix window event bind problem

--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -43,6 +43,9 @@ export default {
   },
   activated () {
     window.addEventListener('scroll', this.handleScroll)
+  },
+  deactivated () {
+    window.removeEventListener('scroll', this.handleScroll)
   }
 }
 </script>

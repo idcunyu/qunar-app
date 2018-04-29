44b3962e4631bfd9e3285f137415be9feed4d13e
vernonn <695035427@qq.com>
2018-04-29 20:06:51 +0800

    change

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -20,7 +20,8 @@ export default {
       swiperOption: {
         pagination: '.swiper-pagination',
         loop: true,
-        autoplay: 3000
+        autoplay: 3000,
+        autoplayDisableOnInteraction: false
       }
     }
   }

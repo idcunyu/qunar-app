2f36726fb2ebade4db73a57a8ecb87432700da9f
vernonn <695035427@qq.com>
2018-04-29 11:17:50 +0800

    fix problem

--- src/router/index.js
+++ src/router/index.js
@@ -24,5 +24,8 @@ export default new Router({
     path: '/detail/:id',
     name: 'Detail',
     component: Detail
-  }]
+  }],
+  scrollBehavior (to, from, savedPosition) {
+    return { x: 0, y: 0 }
+  }
 })
